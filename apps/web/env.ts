357ed2a4712f5ef8a5c1f8dec3c1fd4ae8d9033d
hyoungs <applebmom@daum.net>
2026-07-31 16:58:48 +0900

    [FE] GenAI 환경변수 변경
      - NEXT_PUBLIC_GEMINI_API_KEY 로 변경

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
   API_URL: process.env.NEXT_PUBLIC_API_URL || '',
-  GenAI_KEY: process.env.GEMINI_API_KEY || '',
+  GenAI_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY || '',
 }
