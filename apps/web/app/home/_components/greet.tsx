21e44ed6730ea6c17a4558ef41d433a84b4ab9f7
Sarah <s.lee.bloomy@outlook.com>
2026-08-01 01:42:40 +0900

    [데일리 인사] AI 분석 시 로딩 표시

--- apps/web/app/home/_components/greet.tsx
+++ apps/web/app/home/_components/greet.tsx
@@ -2,8 +2,10 @@
 
 import { useBaseDate } from "@/app/_providers/BaseDateProvider";
 import styles from "@/styles/home/home.module.css";
+import clsx from "clsx";
+import loadingStyles from "@/styles/components/statePage.module.css";
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { ChevronUp, ChevronDown, RotateCw } from "lucide-react";
+import { ChevronUp, ChevronDown, RotateCw, Loader } from "lucide-react";
 
 const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/greet`;
 
@@ -24,10 +26,11 @@ const fetchCommentApi = async (url: string, options: RequestInit, errorMsg: stri
 export default function Greet() {
   const { baseDate, formattedDate: targetDate } = useBaseDate();
   const [nickName, setNickName] = useState<string>('');
+  const [nloading, setNnoading] = useState<boolean>(true);
 
   const [aiComment, setAiComment] = useState<string | null>(null);
-  const [refreshKey, setRefreshKey] = useState<number>(0); // 재생성
   const [loading, setLoading] = useState<boolean>(true);
+  const [showLoader, setShowLoader] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   // 더보기 관련 State & Ref
@@ -37,11 +40,16 @@ export default function Greet() {
 
   // nickName 조회
   useEffect(() => {
+    setLoading(true);
     fetch(`${API_BASE_URL}/name`, { credentials: "include" })
       .then((res) => res.ok ? res.json() : Promise.reject())
-      .then((data: { nickName: string }) => setNickName(data.nickName))
+      .then((data: { nickName: string }) => {
+        setNickName(data.nickName)
+        setNnoading(false);
+      })
       .catch(() => {
         setNickName('자랑스러운 우리 회원');
+        setNnoading(false);
       });
   }, []);
 
@@ -54,8 +62,7 @@ export default function Greet() {
 
       const comment = await fetchCommentApi(
         `${API_BASE_URL}?dailyDate=${targetDate}`,
-        { method: "GET" },
-        "데이터를 불러오지 못했어요."
+        { method: "GET" }, ""
       );
       setAiComment(comment);
     } catch (err) {
@@ -92,6 +99,15 @@ export default function Greet() {
     fetchDailyComment();
   }, [fetchDailyComment]);
 
+  // 로딩 지연
+  useEffect(() => {
+    if (loading) {
+      const timer = setTimeout(() => setShowLoader(true), 1000);
+      return () => clearTimeout(timer);
+    }
+    setShowLoader(false);
+  }, [loading]);
+
   // 텍스트가 5줄을 초과하는지 체크
   useEffect(() => {
     if (aiComment && commentRef.current) {
@@ -109,27 +125,29 @@ export default function Greet() {
     <div className={styles.greetingWrapper}>
       <p className={styles.greeting}>오늘도 반가워요,</p>
       <p className={styles.nickname}>
-        <span><strong>{nickName} </strong>님!</span>
-        {loading ? (
-          <span></span>
-        ) : (<button
-          type="button"
-          className={styles.refreshBtn}
-          onClick={handleRegenerate} /* 실행할 재생성 함수 */
-          disabled={loading}        /* 로딩 중 중복 클릭 방지 */
-          aria-label="AI 건강분석 다시 생성"
-        >
-          <RotateCw
-            size={16}
-            className={`${styles.refreshIcon} ${loading ? styles.spinning : ''}`}
-          />
-        </button>)}
+        {nloading ? <span></span> : (<span><strong>{nickName} </strong>님!</span>)}
+        {loading ? (<span></span>)
+          : (<button
+            type="button"
+            className={styles.refreshBtn}
+            onClick={handleRegenerate} /* 실행할 재생성 함수 */
+            disabled={loading}        /* 로딩 중 중복 클릭 방지 */
+            aria-label="AI 건강분석 다시 생성"
+          >
+            <RotateCw
+              size={16}
+              className={`${styles.refreshIcon} ${loading ? styles.spinning : ''}`}
+            />
+          </button>)}
       </p>
 
       <div className={styles.aiComment}>
-        {loading ? (
-          <span></span>
-        ) : error || !aiComment ? (
+        {showLoader ? (
+          <div className={loadingStyles.icon}>
+            <Loader size={32} className={styles.loading} />
+            <div className={styles.description}>링크케어 AI가 데이터를 분석하고 있어요...</div>
+          </div>
+        ) : error ? (
           <span>데이터를 못 불러왔어요.</span>
         ) : (
           <div
