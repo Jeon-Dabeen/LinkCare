6664e889b89ce23c24faa4b2fbb576ef44e92614
오유진 <ranen12@naver.com>
2026-08-02 22:24:38 +0900

    [매일관리- 체중] backend 체중 로그인인증 추가
      - weight controller jwtAuthGuard 추가
      - service logger 오타 수정

--- apps/api/src/weight/weight.controller.ts
+++ apps/api/src/weight/weight.controller.ts
@@ -1,29 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Query } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Query, UseGuards } from "@nestjs/common";
 import { WeightService } from "./weight.service";
 import { CreateWeightDto } from "./dto/create-weight.dto";
 import { UpdateWeightProfileDto } from "./dto/update-weight-profile-dto";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth/jwt-auth.guard";
+import { CurrentUser } from "../common/decorator/current-user.decorator";
 
+@UseGuards(JwtAuthGuard)
 @Controller("weight")
 export class WeightController {
   constructor(private readonly weightService: WeightService) {}
 
-  @Post(":id")
-  create(@Param("id") id: string, @Body() createWeightDto: CreateWeightDto) {
-    return this.weightService.createWeight(+id, createWeightDto);
+  @Post()
+  create(
+    @CurrentUser("id") userId: number, 
+    @Body() createWeightDto: CreateWeightDto) {
+    return this.weightService.createWeight(Number(userId), createWeightDto);
   }
 
-  @Get("week/:id")
-  findWeek(@Param("id") id: string, @Query("date") date: string) {
-    return this.weightService.findWeekWeight(+id, date);
+  @Get("week")
+  findWeek(
+    @CurrentUser("id") userId: number,
+    @Query("weightDate") weightDate: string,
+  ) {
+    return this.weightService.findWeekWeight(Number(userId), weightDate);
   }
 
-  @Get("month/:id")
-  findMonth(@Param("id") id: string, @Query("date") date: string) {
-    return this.weightService.findMonthWeight(+id, date);
+  @Get("month")
+  findMonth(
+    @CurrentUser("id") userId: number,
+    @Query("date") date: string
+  ) {
+    return this.weightService.findMonthWeight(Number(userId), date);
   }
 
-  @Patch("profile/:id")
-  updateWeightProfile(@Param("id") id: string, @Body() dto: UpdateWeightProfileDto) {
-    return this.weightService.updateWeightProfile(+id, dto);
+  @Patch("profile")
+  updateWeightProfile(
+    @CurrentUser("id") userId: number,
+    @Body() dto: UpdateWeightProfileDto
+  ) {
+    return this.weightService.updateWeightProfile(Number(userId), dto);
   }
 }
