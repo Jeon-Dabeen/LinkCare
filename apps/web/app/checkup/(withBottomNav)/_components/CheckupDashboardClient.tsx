42171b963a9e67a4bdc5b1566f19c27acc201861
kongdeis <kongdeis@outlook.com>
2026-08-05 10:22:22 +0900

    [UI-checkup] 건강검진 메인화면 스타일 수정
      - 연도 탭버튼 수정
      - 빈 화면 EmptyPage 컴포넌트로 변경

--- apps/web/app/checkup/(withBottomNav)/_components/CheckupDashboardClient.tsx
+++ apps/web/app/checkup/(withBottomNav)/_components/CheckupDashboardClient.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import commonStyle from "@/styles/common.module.css";
+import styles from "@/styles/checkup/checkup.module.css";
 import Grid from "@/app/_components/ui/Grid";
 import Button from "@/app/_components/ui/Button";
 import { CheckupAI } from "./CheckupAI";
@@ -9,7 +10,9 @@ import { CheckupData } from "./CheckupData";
 // 기존 page.tsx에 있던 Interface 사용
 import { CheckupDashBoardResponse } from "@/types/checkup";
 import Link from "next/link";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, FileQuestionMark } from "lucide-react";
+import clsx from "clsx";
+import EmptyPage from "@/app/_components/ui/EmptyPage";
 
 interface Props {
   checkupList: CheckupDashBoardResponse[];
@@ -20,50 +23,67 @@ export function CheckupDashboardClient({ checkupList }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   if (!checkupList || checkupList.length === 0) {
-    return <div>검진 데이터가 없습니다.</div>;
+
+    return (
+      <EmptyPage
+        icon={<FileQuestionMark size={32} />}
+        title="검진 데이터가 없어요"
+      />
+    )
   }
 
   // 현재 선택된 검진 데이터
   const selectedData = checkupList[selectedIndex];
 
   if (!selectedData) {
-    return <div>선택된 검진 데이터를 찾을 수 없습니다.</div>;
+    return (
+      <EmptyPage
+        icon={<FileQuestionMark size={32} />}
+        title="선택된 검진 데이터를 찾을 수 없어요"
+      />
+    )
   }
 
   return (
-    <div>
+    <>
       <header className={commonStyle.pageTitleWrapper}>
         <div className={commonStyle.left}>
           <h2 className={commonStyle.pageTitle}>건강검진</h2>
         </div>
-        <div className={commonStyle.right}>
-          {checkupList.map((item, index) => {
-            const label = `${item.year}`;
-
-            return (
-              <Button
-                key={item.id}
-                variant={selectedIndex === index ? "text-primary" : "text-secondary"}
-                onClick={() => setSelectedIndex(index)}
-              >
-                {label}
-              </Button>
-            );
-          })}
-          <Link href="/checkup/upload">
-            <Button variant="text-primary">
-              <CirclePlus />
-              {/* <span>건강검진 데이터 추가</span> */}
-            </Button>
-          </Link>
-        </div>
+        <Link href="/checkup/upload">
+          <Button variant="text-primary">
+            <CirclePlus />
+            <span>건강검진 데이터 추가</span>
+          </Button>
+        </Link>
       </header>
+
+      <div className={styles.yearsButton}>
+        {[...checkupList].reverse().map((item, index) => {
+          const label = `${item.year}`;
+
+          return (
+            <button
+              key={item.id}
+              type="button"
+              className={clsx(
+                styles.year,
+                selectedIndex === index && styles.active
+              )}
+              // variant={selectedIndex === index ? "text-primary" : "text-secondary"}
+              onClick={() => setSelectedIndex(index)}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
 
       {/* 선택된 데이터로 컴포넌트 호출 */}
       <Grid>
         <CheckupAI id={selectedData.id} />
         <CheckupData {...selectedData} />
       </Grid>
-    </div>
+    </>
   );
 }
