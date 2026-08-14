0198734742d3d647b7a901f9197e128d0ef35470
hyoungs <applebmom@daum.net>
2026-08-14 09:56:59 +0900

    [식사] 칼로리, 목표칼로리 숫자입력 수정
      - 칼로리 입력시 빈값일 때 0이 계속 입력되는 현상 수정

--- apps/web/app/meal/record/_components/foodItem.tsx
+++ apps/web/app/meal/record/_components/foodItem.tsx
@@ -23,6 +23,33 @@ export default function FoodItem({
   onDelete,
   onChange,
 }: FoodItemProps) {
+
+  const MAX_UINT_CALORIE = 6000;
+
+  const onChangeCalorie = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+
+    // 빈 값이 입력된 경우
+    if(val == ""){
+      onChange("calorie", "");
+      return;
+    }
+
+    // 00 or 000
+    if(Number(val) === 0 && val.includes("0")){
+      onChange("calorie", "");
+      return;
+    }
+
+    const numericValue = Number(val);
+
+    // 최대값 제한
+    const clampedValue = Math.min(numericValue, MAX_UINT_CALORIE);
+
+    onChange("calorie", clampedValue);
+  }
+
+
   return (
     <li className={styles.inputItem}>
       {canModify && (
@@ -44,7 +71,7 @@ export default function FoodItem({
           unit="kcal"
           value={calorie ?? ""}
           disabled={!canModify}
-          onChange={(e) => onChange("calorie", Number(e.target.value))}
+          onChange={onChangeCalorie}
         />
       </div>
     </li>
