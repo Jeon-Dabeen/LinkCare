033bca1dbaf3810372dd11e56082ce2fc19a587a
Sarah <s.lee.bloomy@outlook.com>
2026-08-19 21:14:15 +0900

    토큰 사라지는 현상 수정 2 - middleware 수정

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -6,10 +6,32 @@ import DailyShield from "./_components/dailyShield";
 import Daily from "./_components/daily";
 import { useBaseDate } from "@/app/_providers/BaseDateProvider";
 import Greet from "./_components/greet";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { ENV } from "@/env";
 
 export default function Home() {
+  const router = useRouter();
   const { baseDate, formattedDate } = useBaseDate();
   console.log(`baseDate: ${baseDate}, formattedDate: ${formattedDate}`);
+
+  useEffect(() => {
+    // 2. 홈에 들어오자마자 인증 체크
+    fetch(`${ENV.API_URL}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+    .then((res) => {
+      if (!res.ok) {
+        // 인증 실패 시 로그인 페이지로 강제 이동
+        router.push("/auth/login");
+      }
+    })
+    .catch(() => {
+      router.push("/auth/login");
+    });
+  }, [router]);
 
   return (
     <section className={commonStyle.mainContent}>
